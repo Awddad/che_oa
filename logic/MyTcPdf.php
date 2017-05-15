b0bb8852684eaef6d6ef8de7285f61122174e094
Administrator <Administrator@XB-201607131754>
2017-05-15 18:54:55 +0800

    增加还款申请和借款申请的pdf功能

--- logic/MyTcPdf.php
+++ logic/MyTcPdf.php
@@ -7,8 +7,11 @@ namespace app\logic;
  */
 use TCPDF;
 class MyTcPdf {
+    
     /**
-     * 
+     * @功能：              生成报销单PDF文件
+     * @作者：              王雕
+     * @创建时间：          2017-05-15
      * @param array $strSaveName   保存的文件名 
      * @param array $arrInfo       格式：$arrInfo = [
                                         'apply_date' => '2017年5月5日',
@@ -35,27 +38,6 @@ class MyTcPdf {
         {
             return false;//文件类型不是pdf
         }
-        //参考格式
-        /*
-        $arrInfo = [
-            'apply_date' => '2017年5月5日',
-            'apply_id' => '20170505102037012134', 
-            'org_full_name' => '南京汽车销售有限公司-中规车一区-涟水店', 
-            'person' => '马聪', 
-            'bank_name' => '中国银行丰庄支行', 
-            'bank_card_id' => '622262132132141241451', 
-            'list' => [
-                [
-                    'type_name' => '差旅费', 
-                    'money' => '1182.00', 
-                    'detail' => '两天的差旅费，具体项目见附件明细'
-                ],
-                //....
-            ], 
-            'approval_person' => '陈贵、李财',//多个人、分隔 
-            'copy_person' => '张三',//多个人、分隔 
-        ];
-        */
         // html 具体样式等前端提供，此处先写个demo
         $strListHtml = '<tr><th>类别</th><th>金额</th><th>明细</th></tr>';
         foreach($arrInfo['list'] as $val)
@@ -96,6 +78,14 @@ class MyTcPdf {
     <div>
         <table>
             <tr>
+                <td>备注信息</td>
+                <td>{$arrInfo['tips']}</td>
+            </tr>
+        </table>
+    </div>
+    <div>
+        <table>
+            <tr>
                 <td>审批人</td>
                 <td>{$arrInfo['approval_person']}</td>
                 <td>抄送人</td>
@@ -114,4 +104,193 @@ TABLEHTML;
         $pdf->lastPage();
         $pdf->Output($strSaveName, 'F');//只保存 F    保存与输出 FI 只输出I
     }
+    
+    
+    /**
+     * @功能：              生成借款单PDF文件
+     * @作者：              王雕
+     * @创建时间：          2017-05-15
+     * @param array $strSaveName   保存的文件名 
+     * @param array $arrInfo       格式：$arrInfo = [
+                                        'apply_date' => '2017年5月5日',
+                                        'apply_id' => '20170505102037012134', 
+                                        'org_full_name' => '南京汽车销售有限公司-中规车一区-涟水店', 
+                                        'person' => '马聪', 
+                                        'bank_name' => '中国银行丰庄支行', 
+                                        'bank_card_id' => '622262132132141241451', 
+                                        'money' => '1265.00', 
+                                        'detail' => '还款测试', 
+                                        'tips' => '你小样', 
+                                        'approval_person' => '陈贵、李财',//多个人、分隔 
+                                        'copy_person' => '张三',//多个人、分隔 
+                                    ];
+     */
+    public function createJieKuanDanPdf($strSaveName, $arrInfo)
+    {
+        if(pathinfo($strSaveName, PATHINFO_EXTENSION) != 'pdf')
+        {
+            return false;//文件类型不是pdf
+        }
+        $strHtml = <<<TABLEHTML
+<style>
+</style>
+<div>
+    <div>
+        <table>
+            <tr>
+                <td>日期</td>
+                <td>{$arrInfo['apply_date']}</td>
+                <td>单号</td>
+                <td>{$arrInfo['apply_id']}</td>
+            </tr>
+            <tr>
+                <td>部门</td>
+                <td>{$arrInfo['org_full_name']}</td>
+                <td>报销人</td>
+                <td>{$arrInfo['person']}</td>
+            </tr>
+            <tr>
+                <td>开户行名称</td>
+                <td>{$arrInfo['bank_name']}</td>
+                <td>银行卡号</td>
+                <td>{$arrInfo['bank_card_id']}</td>
+            </tr>
+        </table>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <th>借款金额</th>
+                <th>事由</th>
+            </tr>
+            <tr>
+                <td>￥{$arrInfo['money']}</td>
+                <td>{$arrInfo['detail']}</td>
+            </tr>
+            <tr>
+                <td>备注</td>
+                <td>{$arrInfo['tips']}</td>
+            </tr>
+        </table>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <td>审批人</td>
+                <td>{$arrInfo['approval_person']}</td>
+                <td>抄送人</td>
+                <td>{$arrInfo['copy_person']}</td>
+                <td>财务确认</td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</div>
+TABLEHTML;
+        $pdf = new TCPDF();
+        $pdf->SetFont('STSongStdLight');//设置宋体，避免中文乱码
+        $pdf->AddPage();
+        $pdf->writeHTML($strHtml, true, false, true, false, '');
+        $pdf->lastPage();
+        $pdf->Output($strSaveName, 'F');//只保存 F    保存与输出 FI 只输出I    
+    }
+    
+/**
+     * @功能：              生成报销单PDF文件
+     * @作者：              王雕
+     * @创建时间：          2017-05-15
+     * @param array $strSaveName   保存的文件名 
+     * @param array $arrInfo       格式：$arrInfo = [
+                                        'apply_date' => '2017年5月5日',
+                                        'apply_id' => '20170505102037012134', 
+                                        'org_full_name' => '南京汽车销售有限公司-中规车一区-涟水店', 
+                                        'person' => '马聪', 
+                                        'bank_name' => '中国银行丰庄支行', 
+                                        'bank_card_id' => '622262132132141241451', 
+                                        'list' => [
+                                            [
+                                                'type_name' => '差旅费', 
+                                                'money' => '1182.00', 
+                                                'detail' => '两天的差旅费，具体项目见附件明细'
+                                            ],
+                                            //....
+                                        ], 
+                                        'tips' => '备注信息', 
+                                        'approval_person' => '陈贵、李财',//多个人、分隔 
+                                        'copy_person' => '张三',//多个人、分隔 
+                                    ];
+     */
+    public function createHuanKuanDanPdf($strSaveName, $arrInfo)
+    {
+        if(pathinfo($strSaveName, PATHINFO_EXTENSION) != 'pdf')
+        {
+            return false;//文件类型不是pdf
+        }
+        // html 具体样式等前端提供，此处先写个demo
+        $strListHtml = '<tr><th>类别</th><th>金额</th><th>明细</th></tr>';
+        foreach($arrInfo['list'] as $val)
+        {
+            $strListHtml .= "<tr><td>{$val['type_name']}</td><td>{$val['money']}</td><td>{$val['detail']}</td></tr>";
+        }
+        $strHtml = <<<TABLEHTML
+<style>
+</style>
+<div>
+    <div>
+        <table>
+            <tr>
+                <td>日期</td>
+                <td>{$arrInfo['apply_date']}</td>
+                <td>单号</td>
+                <td>{$arrInfo['apply_id']}</td>
+            </tr>
+            <tr>
+                <td>部门</td>
+                <td>{$arrInfo['org_full_name']}</td>
+                <td>报销人</td>
+                <td>{$arrInfo['person']}</td>
+            </tr>
+            <tr>
+                <td>开户行名称</td>
+                <td>{$arrInfo['bank_name']}</td>
+                <td>银行卡号</td>
+                <td>{$arrInfo['bank_card_id']}</td>
+            </tr>
+        </table>
+    </div>
+    <div>
+        <table>
+            {$strListHtml}
+        </table>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <td>备注信息</td>
+                <td>{$arrInfo['tips']}</td>
+            </tr>
+        </table>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <td>审批人</td>
+                <td>{$arrInfo['approval_person']}</td>
+                <td>抄送人</td>
+                <td>{$arrInfo['copy_person']}</td>
+                <td>财务确认</td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</div>
+TABLEHTML;
+        $pdf = new TCPDF();
+        $pdf->SetFont('STSongStdLight');//设置宋体，避免中文乱码
+        $pdf->AddPage();
+        $pdf->writeHTML($strHtml, true, false, true, false, '');
+        $pdf->lastPage();
+        $pdf->Output($strSaveName, 'F');//只保存 F    保存与输出 FI 只输出I
+    }
+    
 }
--- modules/oa_v1/controllers/DefaultController.php
+++ modules/oa_v1/controllers/DefaultController.php
@@ -5,6 +5,7 @@ namespace app\modules\oa_v1\controllers;
 use Yii;
 use app\modules\oa_v1\logic\PersonLogic;
 use app\modules\oa_v1\logic\TreeTagLogic;
+use app\models\Menu;
 
 
 /**
@@ -34,14 +35,15 @@ class DefaultController extends BaseController
     }
     
     /**
-     * 获取用户信息接口
+     * 获取用户信息接口 - 网站进入的时候调用该接口
      * @return type
      */
     public function actionGetUserInfo()
     {
         $this->arrPersonInfo;
         $arrData = [
-            'userinfo' => $this->arrPersonInfo->toArray()
+            'userinfo' => $this->arrPersonInfo->toArray(),
+            'menu' => $this->arrPersonRoleInfo['menu']
         ];
         return $this->_return($arrData);
     }
@@ -75,9 +77,56 @@ class DefaultController extends BaseController
         }
     }
 
-    public function actionOrg()
+    /**
+     * 单点登录跳回来的时候带上角色id，设置登录的角色信息
+     * 单点登录的时候跳回来的时候不支持url中有 -  所以此处全部小写
+     */
+    public function actionSetroleinfo()
     {
-        $data = PersonLogic::instance()->getOrgs();
-        return $this->_return($data);
+        $intRoleId = intval(Yii::$app->request->get('role_id'));
+        $arrRoleIds = explode(',', $this->arrPersonInfo->role_ids);
+        //但用户只有一个角色的时候进入系统没有role_id参数
+        if(empty($intRoleId) && count($arrRoleIds) == 1)
+        {
+            $intRoleId = $arrRoleIds[0];
+        }
+        //设置权限
+        if( $intRoleId 
+            && in_array($intRoleId, explode(',', $this->arrPersonInfo->role_ids)) //用户有该角色
+            && $this->setUserRoleInfo($intRoleId, 'web',  true)) //设置角色信息成功
+        {
+            //保存session
+            $session = Yii::$app->getSession();
+            $session->set('role_id', $intRoleId);
+            //设置权限成功 - 跳转到网站首页
+            header('Location: /oa/index.html');
+            exit();
     }
+        else
+        {
+            //失败，跳到登录页面,重新选择权限
+            header('Location: ' . Yii::$app->params['quan_xian']['auth_sso_login_url']);
+            exit();
+}
+    }
+    
+    //获取网站的全部目录
+    public function actionGetAllMenu()
+    {
+        $list = Menu::find()->asArray()->all();
+        return $this->_return($list);
+    }
+    
+    /**
+     * 退出登录
+     */
+    public function actionLoginOut()
+    {
+        //跳转到登出页面
+        Yii::$app->getSession()->destroy();
+        header('Location: ' . Yii::$app->params['quan_xian']['auth_sso_login_url']);
+        exit();
+    }
+    
+    
 }
