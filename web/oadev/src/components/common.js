5631f137aca746838d6ab6bd966bb74e5bce6025
cjh <cjh@youmeng.com>
2017-05-18 16:43:38 +0800

    fix

--- web/oadev/src/components/common.js
+++ web/oadev/src/components/common.js
@@ -3,7 +3,7 @@ import WebStorage from 'react-webstorage';
 const webStorage = new WebStorage(window.sessionStorage || window.localStorage);
 
 export const chkPms = (pids)=>{
-   return true;
+  return true;
   let hasPms = false;
   let _value = webStorage.getItem('adminPms');
   if(!(pids instanceof Array)) return hasPms;
@@ -44,4 +44,36 @@ export const chkPmsForInline = (pids,style)=>{
 export const chkPmsForInlineBlock = (pids,style)=>{
   style = style || {};
   return {...style,display:chkPms(pids)?'inline-block':'none'};
+}
+
+/**
+ * Cookie
+ * 设置Cookie
+ */
+export const setCookie = (name,value) =>{
+    let exp = new Date();
+        exp.setTime(exp.getTime() + 30*60*1000);
+        document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+}
+
+/**
+ * Cookie
+ * 读取Cookie内容
+ */
+export const getCoookie = (name)=>{
+    let arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+        if(arr=document.cookie.match(reg))
+            return unescape(arr[2]);
+        else
+            return null;
+}
+/**
+ * Cookie
+ * 删除Cookie
+ */
+export const delCookie = (name)=>{
+    let exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+    let cval=getCookie(name);
+        if(cval!=null) document.cookie= name + "="+cval+";expires="+exp.toGMTString();
 }
