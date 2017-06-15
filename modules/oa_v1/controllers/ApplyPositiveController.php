59db5c89b0f32c42756939bf442e067eb9b7aeee
jren <602598484@qq.com>
2017-06-15 18:05:38 +0800

    修改

--- modules/oa_v1/controllers/ApplyPositiveController.php
+++ modules/oa_v1/controllers/ApplyPositiveController.php
@@ -17,7 +17,7 @@ class ApplyPositiveController extends BaseController
 		}
 		$res = $model->save($this->arrPersonInfo);
 		if($res['status']){
-			return $this->_return('成功');
+			return $this->_return($res['apply_id']);
 		}else{
 			return $this->_returnError(400,$res['msg']);
 		}
