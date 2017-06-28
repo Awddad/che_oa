cfa4b33386ea93af4b7b9de3c48fc7bdb9767f6f
xjun <xiongjunceplj@163.com>
2017-06-28 13:58:09 +0800

    增加crm

--- modules/oa_v1/controllers/LoginController.php
+++ modules/oa_v1/controllers/LoginController.php
@@ -12,6 +12,7 @@ namespace app\modules\oa_v1\controllers;
 use app\models\Person;
 use Jasny\SSO\Broker;
 use Yii;
+use yii\helpers\ArrayHelper;
 use yii\rest\Controller;
 
 /**
@@ -29,27 +30,39 @@ class LoginController extends Controller
     {
         $session = Yii::$app->session;
         $objPerson = $session->get('USER_INFO');
+        $param = Yii::$app->request->get();
+        $osType = ArrayHelper::getValue($param, 'os_type', 'web');
         if (empty($objPerson)) {
-            $serverUrl = Yii::$app->params['quan_xian']['auth_sso_url'];//单点登录地址
-            $brokerId = Yii::$app->params['quan_xian']['auth_broker_id'];//项目appID
-            $brokerSecret = Yii::$app->params['quan_xian']['auth_broker_secret'];//配置的项目 Secret
-            $loginUrl = Yii::$app->params['quan_xian']['auth_sso_login_url'];
-            $broker = new Broker($serverUrl, $brokerId, $brokerSecret);
-            $broker->attach(true);
-            $user = $broker->getUserInfo();//获取用户信息，这里会curl单点登录获取用户信息,但是不全
-            if (!$user) {
-                //用户没有登录 需要跳转到登录页面去登录
-                $broker->clearToken();
-                header("Location:" . $loginUrl);
-                die();
+            if($osType == 'web') {
+                $serverUrl = Yii::$app->params['quan_xian']['auth_sso_url'];//单点登录地址
+                $brokerId = Yii::$app->params['quan_xian']['auth_broker_id'];//项目appID
+                $brokerSecret = Yii::$app->params['quan_xian']['auth_broker_secret'];//配置的项目 Secret
+                $loginUrl = Yii::$app->params['quan_xian']['auth_sso_login_url'];
+                $broker = new Broker($serverUrl, $brokerId, $brokerSecret);
+                $broker->attach(true);
+                $user = $broker->getUserInfo();//获取用户信息，这里会curl单点登录获取用户信息,但是不全
+                if (!$user) {
+                    //用户没有登录 需要跳转到登录页面去登录
+                    $broker->clearToken();
+                    header("Location:" . $loginUrl);
+                    die();
+                }
+                $objPerson = Person::findOne(['person_id' => $user['id']]);
+                if (empty($objPerson)) {
+                    $broker->clearToken();
+                    header("Location:" . $loginUrl);
+                    die();
+                }
+                $session->set('USER_INFO', $objPerson);
+            } elseif ($osType == 'crm') {
+                $uid = Yii::$app->request->get('uid');
+                $time = Yii::$app->request->get('time');
+                $sign = Yii::$app->request->get('sign');
+                if ($sign == md5($osType.$uid.$time.'che.com')) {
+                    $objPerson = Person::findOne(['person_id' => $uid]);
+                    $session->set('USER_INFO', $objPerson);
+                }
             }
-            $objPerson = Person::findOne(['person_id' => $user['id']]);
-            if (empty($objPerson)) {
-                $broker->clearToken();
-                header("Location:" . $loginUrl);
-                die();
-            }
-            $session->set('USER_INFO', $objPerson);
         }
         $intRoleId = intval(Yii::$app->request->get('role_id'));
         $arrRoleIds = explode(',', $objPerson->role_ids);
