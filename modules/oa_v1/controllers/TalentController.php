34560b7d486686e590bb589f6ff25201ea7554f4
jren <602598484@qq.com>
2017-07-03 18:41:37 +0800

    修改

--- modules/oa_v1/controllers/TalentController.php
+++ modules/oa_v1/controllers/TalentController.php
@@ -5,6 +5,7 @@ use yii;
 use app\modules\oa_v1\models\TalentForm;
 use app\models\Educational;
 use app\models\PersonType;
+use app\modules\oa_v1\models\TalentInfoForm;
 
 /**
  * 招聘 人才
@@ -145,5 +146,79 @@ class TalentController extends BaseController
             ];
         }
         return $this->_return($data);
+    }
+    
+    /**
+     * 修改人才个人信息
+     */
+    public function actionEditInfo()
+    {
+        $post = yii::$app->request->post();
+        $model = new TalentInfoForm();
+        $model->setScenario($model::SCENARIO_TALENT_EDIT);
+        $model->load(['TalentInfoForm'=>$post]);
+        if(!$model->validate()){
+            return $this->_returnError(403,current($model->getFirstErrors()));
+        }
+        $res = $model->saveTalent($this->arrPersonInfo);
+        if($res['status']){
+            return $this->_return('成功');
+        }else{
+            return $this->_returnError(400,$res['msg']);
+        }
+    }
+    
+    /**
+     * 修改人才应聘信息
+     */
+    public function actionEditYingpin()
+    {
+        $post = yii::$app->request->post();
+        $model = new TalentInfoForm();
+        $model->setScenario($model::SCENARIO_TALENT_YINGPIN_EDIT);
+        $model->load(['TalentInfoForm'=>$post]);
+        if(!$model->validate()){
+            return $this->_returnError(403,current($model->getFirstErrors()));
+        }
+        $res = $model->saveYingpin($this->arrPersonInfo);
+        if($res['status']){
+            return $this->_return('成功');
+        }else{
+            return $this->_returnError(400,$res['msg']);
+        }
+    }
+    /**
+     * 获得人才个人信息
+     */
+    public function actionGetInfo()
+    {
+        $id = yii::$app->request->get('id');
+        if($id){
+            $model = new TalentInfoForm();
+            $res = $model->getTalentInfo($id);
+            if($res['status']){
+                return $this->_return($res['data']);
+            }else{
+                return $this->_returnError(400,$res['msg']);
+            }
+        }
+        return $this->_returnError(403,'id不能为空');
+    }
+    /**
+     * 获得人才应聘信息
+     */
+    public function actionGetYingpin()
+    {
+        $id = yii::$app->request->get('id');
+        if($id){
+            $model = new TalentInfoForm();
+            $res = $model->getYingpin($id);
+            if($res['status']){
+                return $this->_return($res['data']);
+            }else{
+                return $this->_returnError(400,$res['msg']);
+            }
+        }
+        return $this->_returnError(403,'id不能为空');
     }
 }
