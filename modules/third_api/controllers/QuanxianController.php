28f750c0b854620a49219bd5cd8c5939c04e74da
xjun <xiongjunceplj@163.com>
2017-07-07 17:35:19 +0800

    up

--- modules/third_api/controllers/QuanxianController.php
+++ modules/third_api/controllers/QuanxianController.php
@@ -5,6 +5,7 @@ namespace app\modules\third_api\controllers;
  * @作者：王雕
  * @创建时间：2017-05-17
  */
+use app\modules\oa_v1\logic\TreeTagLogic;
 use Yii;
 use yii\filters\auth\CompositeAuth;
 use yii\filters\ContentNegotiator;
@@ -29,6 +30,7 @@ class QuanxianController extends Controller
         }
         
         $objQx = new QuanXianServer();
+        $intResult = 0;
         switch($strType)
         {
             //组织架构相关变动
@@ -36,30 +38,31 @@ class QuanxianController extends Controller
             case 'organizations/types'://组织架构类型
                 $intResult = $objQx->curlUpdateOrg();
                 break;
-            
-            //项目人员信息变动
-            case 'users'://用户列表
-            	$intResult = $objQx->curlUpdateAllUser();
-            	break;
             case 'organizations/positions'://职位
             	$intResult = $objQx->curlUpdatePositions();
             	break;
+            case 'users'://用户列表
             case 'projects/users'://项目用户
-                $intResult = $objQx->curlUpdateUser();
-                $objQx->curlUpdateUserRoleOrgPermission();//用户的数据权限
+                $intResult = $objQx->curlUpdateAllUser() + $objQx->curlUpdateUser();
                 break;
             
             //角色权限目录相关信息变动
             case 'projects/roles'://项目角色
-            case 'projects/permission-tree'://项目菜单权限树形结构
-            case 'projects/role_user':
                 $intResult = $objQx->curlUpdateRole();//角色信息
+                break;
+            case 'projects/permission-tree'://项目菜单权限树形结构
+            case 'projects/permissions'://项目菜单权限树形结构
                 $objQx->curlUpdateMenus();//项目菜单
+                break;
+            case 'projects/role_user':
+                $objQx->curlUpdateUserRoleOrgPermission();//用户的数据权限
                 break;
             //菜单信息变动
         }
-        
-        echo '更新成功，影响数据条数:' . $intResult ? : 0;
+        if ($intResult == 0)
+            echo '更新成功';
+        else
+            echo '更新成功，影响数据条数:' . $intResult;
         die();
     }
 }
