cffe7483cf0e7d12137dabceed667d7f5b4ff11f
jren <602598484@qq.com>
2017-07-13 17:13:37 +0800

    除招聘经理外只能看到自己的招聘

--- modules/oa_v1/controllers/TalentController.php
+++ modules/oa_v1/controllers/TalentController.php
@@ -108,7 +108,7 @@ class TalentController extends BaseController
         $get = yii::$app->request->get();
         $model = new TalentForm();
         
-        $data = $model->getList($get);
+        $data = $model->getList($get,$this->arrPersonInfo,$this->arrPersonRoleInfo);
         
 		return $this->_return($data);
     }
