e4d16f88f44b5a0df1695537bd6ddc8ce7a802e6
xjun <xiongjunceplj@163.com>
2017-07-18 18:01:14 +0800

    fix

--- modules/oa_v1/controllers/BaseController.php
+++ modules/oa_v1/controllers/BaseController.php
@@ -116,7 +116,9 @@ class BaseController extends Controller
         //权限管理
         $roleInfo = Role::findOne($this->roleId);
         // 设置角色别名
-        $this->roleName = $roleInfo->slug;
+        if(!empty($roleInfo)) {
+            $this->roleName = $roleInfo->slug;
+        }
         $roleArr = ArrayHelper::getColumn(json_decode($roleInfo->permissions), 'url');
         $requestUrlArr = explode('?', $_SERVER['REQUEST_URI']);
         $allMenu = ArrayHelper::getColumn(Menu::find()->asArray()->all(), 'url');
