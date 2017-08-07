a10f0c1b2c550bea54fca0c0951823621c38bf60
jren <602598484@qq.com>
2017-08-07 10:06:36 +0800

    修改

--- modules/oa_v1/logic/BaseApplyLogic.php
+++ modules/oa_v1/logic/BaseApplyLogic.php
@@ -262,7 +262,6 @@ class BaseApplyLogic extends Logic
      */
     public function getApplyBase($apply)
     {
-        $approval_persons = $this->getApprovalPersons($apply);
         return [
             'approval_persons' => $this->getApprovalPersons($apply),
             'copy_persons' => $this->getCopyPersons($apply),
