8ea8caeef53c06fdf37662b6446128ad148e46e6
xjun <xiongjunceplj@163.com>
2017-08-10 14:12:13 +0800

    fixed

--- modules/oa_v1/logic/BaseApplyLogic.php
+++ modules/oa_v1/logic/BaseApplyLogic.php
@@ -176,7 +176,7 @@ class BaseApplyLogic extends Logic
                 "name" => $apply->cai_wu_person,
                 "date"=> date('Y-m-d H:i', $apply->cai_wu_time),
                 "org" => PersonLogic::instance()->getOrgNameByPersonId($apply->cai_wu_person_id),
-                "status" => 0,
+                "status" => 3,
                 'diff_time' => $apply->cai_wu_time - $perTime,
                 'des' => $apply->caiwu_refuse_reason,
             ];
@@ -185,7 +185,7 @@ class BaseApplyLogic extends Logic
                 "name" => '',
                 "date"=> date('Y-m-d H:i', $apply->cai_wu_time),
                 "org" => '',
-                "status" => 3,
+                "status" => 0,
                 'diff_time' => $apply->cai_wu_time - $apply->create_time
             ];
         }
