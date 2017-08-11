d523ebcebb5718856b43a331f28186ea5a629f6a
jren <602598484@qq.com>
2017-08-11 10:42:27 +0800

    修改

--- commands/EmployeeController.php
+++ commands/EmployeeController.php
@@ -15,32 +15,29 @@ class EmployeeController extends Controller
 {
     public function actionPositive()
     {
+        $shiyong_id = EmployeeType::findOne(['slug' => 'shiyong'])->id;
+        $zhengshi_id = EmployeeType::findOne(['slug' => 'zhengshi'])->id;
         $success = 0;
-        $fail = 0;
         $query = Apply::find()
-        ->select('a.person_id')
+        ->select('a.person_id,positive_time')
         ->alias('a')
         ->join('LEFT JOIN', ApplyPositive::tableName().' p', 'a.apply_id = p.apply_id')
-        ->where(['a.type' => 10, 'status' => 99]);
+        ->join('LEFT JOIN',Employee::tableName().' e','e.person_id = a.person_id')
+        ->where(['a.type' => 10, 'a.status' => 99,'e.employee_type'=>$shiyong_id]);
         
         //echo $query->createCommand()->getRawSql();die();
         $res = $query->asArray()->all();
         if($res){
-            $shiyong_id = EmployeeType::findOne(['slug' => 'shiyong'])->id;
-            $zhengshi_id = EmployeeType::findOne(['slug' => 'zhengshi'])->id;
+            $person_ids = [];
             foreach($res as $v){
-                $employee = Employee::findOne(['person_id' => $v['person_id'], 'employee_type' => $shiyong_id]);
-                if(empty($employee)){
-                    continue;
-                }
-                $employee->employee_type = $zhengshi_id;
-                if($employee->save()){
-                    $success += 1;
-                }else{
-                    $fail += 1;        
+                if($v['positive_time'] && (strtotime($v['positive_time']) <= time())){
+                    $person_ids[] = $v['person_id'];
                 }
             }
+            if(count($person_ids) > 0){
+                $success = Employee::updateAll(['employee_type'=>$zhengshi_id],['in','person_id',$person_ids]);
+            }
         }
-        echo "success count:{$success}".PHP_EOL."fail count:{$fail}";
+        echo "success count:{$success}";
     }
 }
