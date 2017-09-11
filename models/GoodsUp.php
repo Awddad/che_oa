22cfae9e1eb04ebdd389a74e88330eda5f20c704
xjun <xiongjunceplj@163.com>
2017-09-11 20:19:40 +0800

    fixed

--- models/GoodsUp.php
+++ models/GoodsUp.php
@@ -59,12 +59,8 @@ class GoodsUp extends \yii\db\ActiveRecord
      */
     public function getDesInfo()
     {
-<<<<<<< HEAD
-        return  $this->shop_name ? : '--' . ' - '. $this->des ? : '--';
-=======
         $shopName = $this->shop_name ? : '--';
         $des = $this->des ? : '--';
         return  $shopName. ' - '. $des;
->>>>>>> feature/oa
     }
 }
